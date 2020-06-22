17775dde673f65b182de1501efa06fa52c5c99e5
JinieOH <hijinie20@gmail.com>
2020-06-22 17:37:13 +0900

    프로젝트 생성

---
+++ src/main/java/com/luce/danngn/DanngnApplication.java
@@ -0,0 +1,13 @@
+package com.luce.danngn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanngnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanngnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luce/danngn/DanngnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luce.danngn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanngnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
